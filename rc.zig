cd70a79ebefb221e4d82f6a41683d58f0eefd1cb
mrtowers <dawmc52@gmail.com>
2025-01-27 17:55:00 +0100

    added destroyFn, closes #5

--- rc.zig
+++ rc.zig
@@ -8,6 +8,7 @@ pub fn Rc(comptime T: type) type {
         allocator: Allocator,
         refs: usize,
         value: T,
+        destroyFn: ?*const fn (Allocator, *const T) void,
 
         const Self = @This();
 
@@ -17,6 +18,17 @@ pub fn Rc(comptime T: type) type {
             obj.refs = 1;
             obj.value = value;
             obj.allocator = allocator;
+            obj.destroyFn = null;
+            return obj;
+        }
+
+        const RcExtraOptions = struct {
+            destroyFn: ?*const fn (Allocator, *const T) void,
+        };
+
+        pub fn initExtra(allocator: Allocator, value: T, options: RcExtraOptions) !*Self {
+            var obj = try init(allocator, value);
+            obj.destroyFn = options.destroyFn;
             return obj;
         }
 
@@ -31,14 +43,17 @@ pub fn Rc(comptime T: type) type {
             if (self.refs == 0) return true;
             self.refs -= 1;
             if (self.refs <= 0) {
-                self.allocator.destroy(self);
+                self.destroy();
                 return true;
             }
             return false;
         }
 
-        /// immediately frees object
+        /// immediately frees object, runs destroyFn if not null
         pub fn destroy(self: *const Self) void {
+            if (self.destroyFn) |f| {
+                f(self.allocator, &self.value);
+            }
             self.allocator.destroy(self);
         }
     };
@@ -51,6 +66,7 @@ pub fn RcThreadSafe(comptime T: type) type {
         refs: usize,
         value: T,
         lock: std.Thread.Mutex,
+        destroyFn: ?*const fn (Allocator, *const T) void,
 
         const Self = @This();
 
@@ -61,6 +77,13 @@ pub fn RcThreadSafe(comptime T: type) type {
             obj.value = value;
             obj.allocator = allocator;
             obj.lock = std.Thread.Mutex{};
+            obj.destroyFn = null;
+            return obj;
+        }
+
+        pub fn initExtra(allocator: Allocator, value: T, options: Rc(T).RcExtraOptions) !*Self {
+            var obj = try init(allocator, value);
+            obj.destroyFn = options.destroyFn;
             return obj;
         }
 
@@ -81,7 +104,7 @@ pub fn RcThreadSafe(comptime T: type) type {
 
             if (self.refs <= 0) {
                 self.lock.unlock();
-                self.allocator.destroy(self);
+                self.destroy();
                 return true;
             }
 
@@ -90,8 +113,11 @@ pub fn RcThreadSafe(comptime T: type) type {
             return false;
         }
 
-        /// immediately frees object
+        /// immediately frees object, runs destroyFn if not null
         pub fn destroy(self: *const Self) void {
+            if (self.destroyFn) |f| {
+                f(self.allocator, &self.value);
+            }
             self.allocator.destroy(self);
         }
     };
@@ -137,6 +163,58 @@ test "RcThreadSafe" {
     try testing.expect(obj.deref());
     obj = try RcThreadSafe(u8).init(testing.allocator, 5);
     obj.destroy();
+}
+
+test "DestroyFn" {
+    const testing = std.testing;
+    const allocator = testing.allocator;
+
+    const User = struct {
+        name: []u8,
+
+        var destroyed = false;
+
+        fn destroyFn(ally: Allocator, user: *const @This()) void {
+            ally.free(user.name);
+            destroyed = true;
+        }
+    };
+
+    var ptr = try Rc(User).initExtra(allocator, User{ .name = try allocator.dupe(u8, "Dawid") }, .{
+        .destroyFn = User.destroyFn,
+    });
+    try testing.expect(std.mem.eql(u8, "Dawid", ptr.value.name));
+    _ = ptr.deref();
+    try testing.expect(User.destroyed);
+    const ptr2 = try Rc(u8).init(allocator, 0);
+    try testing.expect(ptr2.destroyFn == null);
+    _ = ptr2.deref();
+}
+
+test "DestroyFnThreadSafe" {
+    const testing = std.testing;
+    const allocator = testing.allocator;
+
+    const User = struct {
+        name: []u8,
+
+        var destroyed = false;
+
+        fn destroyFn(ally: Allocator, user: *const @This()) void {
+            ally.free(user.name);
+            destroyed = true;
+        }
+    };
+
+    var ptr = try RcThreadSafe(User).initExtra(allocator, User{ .name = try allocator.dupe(u8, "Dawid") }, .{
+        .destroyFn = User.destroyFn,
+    });
+    try testing.expect(std.mem.eql(u8, "Dawid", ptr.value.name));
+    _ = ptr.deref();
+    try testing.expect(User.destroyed);
+    const ptr2 = try RcThreadSafe(u8).init(allocator, 0);
+    try testing.expect(ptr2.destroyFn == null);
+    _ = ptr2.deref();
 }
 
 fn thread_func(arg: *RcThreadSafe(u8)) !void {
