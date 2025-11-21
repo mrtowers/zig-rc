5b5b3240353ae6cdc37ff410e6af09cf589f65b0
mrtowers <dawmc52@gmail.com>
2025-11-21 16:44:27 +0100

    added arc
    squash
    
    x

--- rc.zig
+++ rc.zig
@@ -3,26 +3,30 @@ const mem = std.mem;
 const build_options = @import("build_options");
 const Allocator = mem.Allocator;
 
-/// generates reference counting struct depending on options
-pub fn Rc(comptime T: type) type {
-    const Callbacks = struct {
+fn Callbacks(comptime T: type) type {
+    return struct {
         destroyFn: ?*const fn (*T) void = null,
     };
+}
 
-    const Options = struct {
-        callbacks: Callbacks = .{},
+fn Options(comptime T: type) type {
+    return struct {
+        callbacks: Callbacks(T) = .{},
     };
+}
 
+/// generates reference counting struct depending on options
+pub fn Rc(comptime T: type) type {
     return struct {
         allocator: Allocator,
         refs: usize,
         value: T,
-        callbacks: Callbacks,
+        callbacks: Callbacks(T),
 
         const Self = @This();
 
         /// caller owns value and must free it with deref()
-        pub fn init(allocator: Allocator, value: T, extra: Options) !*Self {
+        pub fn init(allocator: Allocator, value: T, extra: Options(T)) !*Self {
             var obj = try allocator.create(Self);
             obj.refs = 1;
             obj.value = value;
@@ -49,8 +53,7 @@ pub fn Rc(comptime T: type) type {
             return false;
         }
 
-        /// immediately frees object, runs destroyFn if not null
-        pub fn deinit(self: *Self) void {
+        fn destroy(self: *Self) void {
             if (build_options.auto_deinit) {
                 if (std.meta.hasMethod(@TypeOf(self.value), "deinit")) {
                     self.value.deinit();
@@ -59,7 +62,58 @@ pub fn Rc(comptime T: type) type {
             if (self.callbacks.destroyFn) |f| {
                 f(&self.value);
             }
+        }
+
+        /// immediately frees object, runs destroyFn if not null
+        pub fn deinit(self: *Self) void {
+            self.destroy();
             self.allocator.destroy(self);
+        }
+    };
+}
+
+fn Arc(comptime T: type) type {
+    return struct {
+        lock: std.Thread.Mutex = .{},
+        rc: *Rc(T),
+
+        const Self = @This();
+
+        pub fn init(allocator: Allocator, value: T, extra: Options(T)) !*Self {
+            const obj = try allocator.create(Self);
+            obj.* = Self{
+                .rc = try Rc(T).init(allocator, value, extra),
+            };
+
+            return obj;
+        }
+
+        pub fn ref(self: *Self) *Self {
+            self.lock.lock();
+            defer self.lock.unlock();
+
+            _ = self.rc.ref();
+            return self;
+        }
+
+        pub fn deref(self: *Self) bool {
+            self.lock.lock();
+
+            const allocator = self.rc.allocator;
+
+            if (self.rc.deref()) {
+                allocator.destroy(self);
+                return true;
+            }
+
+            self.lock.unlock();
+            return false;
+        }
+
+        pub fn deinit(self: *Self) void {
+            const allocator = self.rc.allocator;
+            self.rc.deinit();
+            allocator.destroy(self);
         }
     };
 }
@@ -114,6 +168,26 @@ test "destroyFn" {
     const ptr2 = try Rc(u8).init(allocator, 0, .{});
     try testing.expect(ptr2.callbacks.destroyFn == null);
     _ = ptr2.deref();
+}
+
+test "Arc" {
+    const testing = std.testing;
+    var obj = try Arc(u8).init(testing.allocator, 4, .{});
+    {
+        var obj_list = try std.ArrayList(*Arc(u8)).initCapacity(testing.allocator, 0);
+        defer {
+            for (obj_list.items) |v| _ = v.deref();
+            obj_list.deinit(testing.allocator);
+        }
+        try obj_list.append(testing.allocator, obj.ref());
+        try obj_list.append(testing.allocator, try Arc(u8).init(testing.allocator, 7, .{}));
+        try testing.expectEqual(obj.rc.refs, 2);
+        try testing.expectEqual(obj_list.items[1].rc.refs, 1);
+    }
+    try testing.expectEqual(obj.rc.refs, 1);
+    try testing.expectEqual(obj.deref(), true);
+    obj = try Arc(u8).init(testing.allocator, 5, .{});
+    obj.deinit();
 }
 
 test "auto deinit" {
